ce0090d18308734a87b952004cd5c2322c721974
marinoaidomoan <marinoaidomoan@gmail.com>
2026-04-21 13:48:30 +0100

    Struture

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestion Etudiants</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="assets/js//script.js"></script>
+</head>
+<body>
+    <div>
+        <h2>Gestion des Etudiants</h2><br>
+
+        <form action="traitement.php" method="POST">
+            <input type="hidden">
+            <input type="text" placeholder="Nom">
+            <input type="text" placeholder="Prenom">
+            <select name="" id="">
+                <option value="">filter_id</option>
+            </select>
+        </form>
+    </div>
+</body>
+</html>

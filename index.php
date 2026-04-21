cb365eef5a7460dbf37780fbce04167817070c0e
marinoaidomoan <marinoaidomoan@gmail.com>
2026-04-21 14:33:05 +0100

    ajout etudiant succes

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+    $id="";$nom="";$prenom="";$fil="";
+    $db = new PDO('mysql:host=127.0.0.1;dbname=gestion_etudiants', 'root', '');
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    if(isset($_POST['btn']) && $_POST['btn']=="ajouter"){
+		$db->query('INSERT INTO etudiants(nom,prenom,filiere_id) VALUES("'.$_POST['nom'].'","'.$_POST['prenom'].'","'.$_POST['fil'].'")');
+	}
+
+    $filieres = $db->query("SELECT * FROM filieres");
+?>
 <!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
@@ -10,12 +21,15 @@
     <div id="BE">
         <h2>Gestion des Etudiants</h2><br>
 
-        <form action="traitement.php" method="POST">
+        <form action="index.php" method="POST">
             <input type="hidden" name="id">
-            <input type="text" placeholder="Nom" name="nom"><br>
-            <input type="text" placeholder="Prenom" name="prenom"><br>
+            <input type="text" placeholder="Nom" name="nom" value="<?=$nom ?>"><br>
+            <input type="text" placeholder="Prenom" name="prenom" value="<?=$prenom ?>"><br>
             <select name="fil" id="">
                 <option selected disabled value="">filter_id</option>
+                <?php while($row = $filieres->fetch()): ?>
+			        <option value="<?= $row['id'] ?>"><?= $row['nom'] ?></option>
+		        <?php endwhile; ?>
             </select><br>
             <input type="submit" name="btn" value="ajouter">
         </form>
